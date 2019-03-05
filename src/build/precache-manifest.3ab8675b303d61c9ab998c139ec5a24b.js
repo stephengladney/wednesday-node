beccde75113e941a847574558bbcbb26175e991b
Stephen Gladney <stephengladney@Stephens-Mac-mini.local>
2019-03-05 00:03:31 -0500

    add sort to spotify library

---
+++ src/build/precache-manifest.3ab8675b303d61c9ab998c139ec5a24b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a8f01790fab307f826d",
+    "url": "/static/css/main.4796d9ae.chunk.css"
+  },
+  {
+    "revision": "2a8f01790fab307f826d",
+    "url": "/static/js/main.2a8f0179.chunk.js"
+  },
+  {
+    "revision": "8360ec3eb265c828e39b",
+    "url": "/static/js/1.8360ec3e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e19d4fd5860344cc63feb1b4a3da999",
+    "url": "/index.html"
+  }
+];
--- src/build/service-worker.js
+++ src/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab75df41fb7eccc6ecbb8b92440fae32.js"
+  "/precache-manifest.3ab8675b303d61c9ab998c139ec5a24b.js"
 );
 
 workbox.clientsClaim();
